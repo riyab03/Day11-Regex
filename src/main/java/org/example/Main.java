95d4ebaa027631e4ad8fa47e5c9e94ebb0d73bbb
rbisht03 <riyabisht979@gmail.com>
2024-12-12 11:55:26 +0530

    Valid First Name

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+    //first name
+    public boolean validFirstName(String fName){
+        return fName.matches("^[A-Z][a-zA-Z]{2,}$");
+    }
+
 
     public static void main(String[] args) {
         System.out.println("User Registration Form");
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,20 @@
+import org.example.Main;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MainTest {
+    @Test
+    void testAge() {
+        Main m1 = new Main();
+
+        //first name
+        assertTrue(m1.validFirstName("Riya"));
+        assertFalse(m1.validFirstName("Sh"));
+        assertFalse(m1.validFirstName("riya"));
+        assertTrue(m1.validFirstName("Riy"));
+
+
+    }
 }
+
